35a15b86513a3dc30024ceb879d9be9b4cf76a33
Sugihara Masaki <sugihara0227@gmail.com>
2019-08-21 18:08:47 +0700

    Add Micropost Function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id]) 
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -43,13 +44,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "ログインしてください"
-      redirect_to root_url
-    end
   end
 
   def correct_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 User.create!(name: "Example User",
-            email: "example@example.com",
-            password: "foobar",
-            password_confirmation: "foobar")
+             email: "example@example.com",
+             password: "foobar",
+             password_confirmation: "foobar")
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@example.com"
   password = "password"
-User.create!(name: name,
-            email: email,
-            password: password,
-            password_confirmation: password)
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
 end
 
 users = User.order(:created_at).take(6)
